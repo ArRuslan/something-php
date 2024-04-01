195c756b8da27294f0b2de10c9d53943bb94ce7e
RuslanUC <dev_ruslan_uc@protonmail.com>
2024-04-01 16:25:42 +0300

    apply patch from danyloBiletskyi/frontPage

--- constants.php
+++ constants.php
@@ -1,7 +1,7 @@
 <?php
 
-$FOOTER = "<div class='footer'>
-    <div>
+$FOOTER = "<div class='footer fixed-bottom'>
+    <div class='align-items-center'>
         <p>Contact info: something@something.com</p>
         <p>Copyright 2024 All rights reserved</p>
     </div>
--- index.php
+++ index.php
@@ -8,6 +8,7 @@ switch ($path) {
     case "/index":
         require "HomeWebpage.php";
         $page = new HomeWebpage();
+        $page->setBody(file_get_contents("pages/index/body.html"));
         break;
     case "/auth":
         require "AuthWebpage.php";
@@ -38,10 +39,46 @@ switch ($path) {
     <title><?php echo $page->getTitle() ?></title>
     <link rel="stylesheet" href="/assets/css/main.css"/>
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="/assets/css/home.css"/>
 </head>
 <body>
+    <header class="p-3 colorbg text-white">    
+        <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+            </a>
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li><button class="bdel"><a href="/index" class="nav-link px-2 text-secondary">IDKChat</a></button></li>
+            <li><button class="bdel"><a href="/pages/dialogs/body.php" class="nav-link px-2 text-white">Dialogs</a></button></li>
+            <li><button class="bdel" onclick="loadContent('faq')"><a href="#" class="nav-link px-2 text-white">FAQs</a></button></li>
+            <li><button class="bdel" onclick="loadContent('about')"><a href="#" class="nav-link px-2 text-white">About</a></button></li>
+            </ul>
+            <div class="text-end">
+            <a href="/login.php">
+            <button type="button" class="btn btn-outline-light me-2" onclick="deleteExceptHeader()">Login</button>
+            </a>
+            <button type="button" class="btn btn-warning">Sign-up</button>
+            </div>
+        </div>
+        </div>
+    </header>
+
+    <script>
+        function loadContent(contentType){
+            switch(contentType){
+                case 'faq':
+                    window.location.href='/index?faq=true';
+                    break;
+                case 'about':
+                    window.location.href='/index?about=true';
+                    break;
+                default:
+                window.location.href='/index';
+            }
+        }
+    </script>
 <?php
-echo $page->getHeader();
 echo $page->getBody();
 echo $page->getFooter();
 ?>
