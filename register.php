702d76166315cfec2ac77480fa1c3bb08e5f9d5e
RuslanUC <dev_ruslan_uc@protonmail.com>
2024-04-22 18:04:53 +0300

    add duplicate entry handling

--- register.php
+++ register.php
@@ -9,7 +9,24 @@ include "Database.php";
 
 // TODO: handle errors
 $db = new Database($GLOBALS["db_host"], $GLOBALS["db_user"], $GLOBALS["db_password"], $GLOBALS["db_database"]);
-$db->addUser($_POST["login"], $_POST["password"]);
+try {
+    $db->addUser($_POST["login"], $_POST["password"]);
+} catch (mysqli_sql_exception $e) {
+    if ($e->getCode() == 1062) {
+        die("
+            <html>
+                <head>
+                    <meta http-equiv=\"refresh\" content=\"3;url=/auth\" />
+                </head>
+                <body>
+                    <h1>User with same login already exists!</h1>
+                    <p>Redirecting in 3 seconds...</p>
+                </body>
+            </html>
+        ");
+    }
+}
+
 $db->close();
 
 session_start();
